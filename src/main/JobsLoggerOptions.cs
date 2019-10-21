96370c7bd00e3f51296dbe90d3a8e5ea5dee1210
Felix Clase <felixclase@gmail.com>
2019-10-21 14:44:08 -0400

    Add Color Properties Options

--- src/main/JobsLoggerOptions.cs
+++ src/main/JobsLoggerOptions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Hangfire.JobsLogger
@@ -8,5 +9,17 @@ namespace Hangfire.JobsLogger
     public class JobsLoggerOptions
     {
         public LogLevel LogLevel { get; set; } = LogLevel.Information;
+
+        public Color LogTraceColor { get; set; } = Color.White;
+
+        public Color LogDebugColor { get; set; } = Color.GhostWhite;
+
+        public Color LogInformationColor { get; set; } = Color.Blue;
+
+        public Color LogWarningColor { get; set; } = Color.Orange;
+
+        public Color LogErrorColor { get; set; } = Color.Red;
+
+        public Color LogCriticalColor { get; set; } = Color.DarkRed;
     }
 }
