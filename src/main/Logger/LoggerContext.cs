d8115b28b3e67b03884c0628fa60f3a28af4bc7c
Felix Clase <felixclase@gmail.com>
2019-10-21 11:37:31 -0400

    Base Context.

---
+++ src/main/Logger/LoggerContext.cs
@@ -0,0 +1,37 @@
+﻿using Hangfire.Server;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangfire.JobsLogger.Logger
+{
+    internal class LoggerContext : ILogger
+    {
+        public static LoggerContext FromPerformContext(PerformContext context)
+        {
+            return context?.Items["LoggerContext"] as LoggerContext ?? null;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (!IsEnabled(logLevel))
+            {
+                return;
+            }
+
+
+            
+        }
+    }
+}
