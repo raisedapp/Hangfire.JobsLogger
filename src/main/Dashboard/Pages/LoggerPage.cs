45d202cc612f319a58eee599a50a63f9efeed616
Brayan mota <bamotav@gmail.com>
2019-10-23 16:26:29 -0400

    Add Razor page Logging, Processing State and another utilities

---
+++ src/main/Dashboard/Pages/LoggerPage.cs
@@ -0,0 +1,42 @@
+﻿using Hangfire.Dashboard;
+using Hangfire.Dashboard.Pages;
+using Hangfire.JobsLogger.Helper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangfire.JobsLogger.Dashboard.Pages
+{
+    internal sealed class LoggerPage : RazorPage
+    {
+        public const string Title = "Logging";
+        public const string PageRoute = "/Logger";
+
+        private static readonly string PageHtml;
+
+        static LoggerPage()
+        {
+            PageHtml = Utility.ReadStringResource("Hangfire.JobsLogger.Dashboard.Page.Html.Logger.html");
+        }
+
+        public override void Execute()
+        {
+            WriteEmptyLine();
+            Layout = new LayoutPage(Title);
+            WriteLiteralLine(PageHtml);
+            WriteEmptyLine();
+        }
+
+        private void WriteLiteralLine(string textToAppend)
+        {
+            WriteLiteral(textToAppend);
+            WriteLiteral("\r\n");
+        }
+
+        private void WriteEmptyLine()
+        {
+            WriteLiteral("\r\n");
+        }
+
+    }
+}
---
+++ src/main/Helper/Utility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Resources;
+using System.Text;
+
+namespace Hangfire.JobsLogger.Helper
+{
+    internal static class Utility
+    {
+        const string regexCron = @"^\s*($|#|\w+\s*=|(\?|\*|(?:[0-5]?\d)(?:(?:-|\/|\,)(?:[0-5]?\d))?(?:,(?:[0-5]?\d)(?:(?:-|\/|\,)(?:[0-5]?\d))?)*)\s+(\?|\*|(?:[0-5]?\d)(?:(?:-|\/|\,)(?:[0-5]?\d))?(?:,(?:[0-5]?\d)(?:(?:-|\/|\,)(?:[0-5]?\d))?)*)\s+(\?|\*|(?:[01]?\d|2[0-3])(?:(?:-|\/|\,)(?:[01]?\d|2[0-3]))?(?:,(?:[01]?\d|2[0-3])(?:(?:-|\/|\,)(?:[01]?\d|2[0-3]))?)*)\s+(\?|\*|(?:0?[1-9]|[12]\d|3[01])(?:(?:-|\/|\,)(?:0?[1-9]|[12]\d|3[01]))?(?:,(?:0?[1-9]|[12]\d|3[01])(?:(?:-|\/|\,)(?:0?[1-9]|[12]\d|3[01]))?)*)\s+(\?|\*|(?:[1-9]|1[012])(?:(?:-|\/|\,)(?:[1-9]|1[012]))?(?:L|W)?(?:,(?:[1-9]|1[012])(?:(?:-|\/|\,)(?:[1-9]|1[012]))?(?:L|W)?)*|\?|\*|(?:JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)(?:(?:-)(?:JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC))?(?:,(?:JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)(?:(?:-)(?:JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC))?)*)\s+(\?|\*|(?:[0-6])(?:(?:-|\/|\,|#)(?:[0-6]))?(?:L)?(?:,(?:[0-6])(?:(?:-|\/|\,|#)(?:[0-6]))?(?:L)?)*|\?|\*|(?:MON|TUE|WED|THU|FRI|SAT|SUN)(?:(?:-)(?:MON|TUE|WED|THU|FRI|SAT|SUN))?(?:,(?:MON|TUE|WED|THU|FRI|SAT|SUN)(?:(?:-)(?:MON|TUE|WED|THU|FRI|SAT|SUN))?)*)(|\s)+(\?|\*|(?:|\d{4})(?:(?:-|\/|\,)(?:|\d{4}))?(?:,(?:|\d{4})(?:(?:-|\/|\,)(?:|\d{4}))?)*))$";
+        public static string ReadStringResource(string resourceName)
+        {
+            var assembly = typeof(Utility).Assembly;
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null) throw new MissingManifestResourceException($"Cannot find resource {resourceName}");
+
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+
+        
+        public static string FormatKey(string serverId) => "utilization:" + serverId;
+    }
+}
